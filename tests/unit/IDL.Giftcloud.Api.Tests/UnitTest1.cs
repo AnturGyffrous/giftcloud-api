a10207a5b00cab67b6b29ab9fbf8a8ca3bbd02b5
Owain Williams <owilliams@groupon.com>
2018-10-05 12:18:57 +0100

    Create project and folder structure for Giftcloud API
    Commands executed:
    
        dotnet new sln --name Giftcloud.Api
        mkdir .\src\IDL.Giftcloud.Api
        cd .\src\IDL.Giftcloud.Api\
        dotnet new web
        cd ..\..\
        dotnet sln add .\src\IDL.Giftcloud.Api\IDL.Giftcloud.Api.csproj
        mkdir .\tests\unit\IDL.Giftcloud.Api.Tests
        cd .\tests\unit\IDL.Giftcloud.Api.Tests
        dotnet new xunit
        dotnet add reference ..\..\..\src\IDL.Giftcloud.Api\IDL.Giftcloud.Api.csproj
        cd ..\..\..\
        dotnet sln add .\tests\unit\IDL.Giftcloud.Api.Tests\IDL.Giftcloud.Api.Tests.csproj
        mkdir .\tests\integration\IDL.Giftcloud.Api.IntegrationTests
        cd .\tests\integration\IDL.Giftcloud.Api.IntegrationTests
        dotnet new xunit
        dotnet add reference ..\..\..\src\IDL.Giftcloud.Api\IDL.Giftcloud.Api.csproj
        cd ..\..\..\
        dotnet sln add .\tests\integration\IDL.Giftcloud.Api.IntegrationTests\IDL.Giftcloud.Api.IntegrationTests.csproj

---
+++ tests/unit/IDL.Giftcloud.Api.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace IDL.Giftcloud.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
